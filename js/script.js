c2fbc585dbdeeaabe2ba8f02094144e63e418761
paradox244 <paradoxmen244@gmail.com>
2019-10-21 10:22:12 +0500

    автопрефиксер

--- js/script.js
+++ js/script.js
@@ -51,3 +51,21 @@ window.addEventListener("keydown", function (event) {
         }
     }
 });
+
+var mapBlock = document.querySelector(".map-popap");
+var mapLink = document.querySelector(".company-contacts-maps");
+var mapClose = document.querySelector(".close-map");
+
+if (mapLink) {
+    mapLink.addEventListener("click", function (event) {
+        event.preventDefault(event);
+        mapBlock.classList.add("show-block");
+    });
+}
+
+if (mapClose) {
+    mapClose.addEventListener("click", function (event) {
+        event.preventDefault(event);
+        mapBlock.classList.remove("show-block");
+    });
+}
